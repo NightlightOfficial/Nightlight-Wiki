47d2bc7b712b61dfa9862876e14b48e9bf0359bc
catpawzz <cat@catpawz.net>
2026-02-05 11:10:46 +0100

    Deployment using Github Pages - made the workflow file for github actions - added needed configs to the main docusaurus config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,8 +31,9 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NightlightOfficial', // Usually your GitHub org/user name.
   projectName: 'Nightlight-Wiki', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
